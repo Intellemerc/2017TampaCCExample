e63da7874c62ffb2dfd82a13ebdab280463d5cff
James Armstrong <intellemerc@James-Macbook.lan>
2017-10-22 22:28:59 -0400

    Add example of a component

--- src/TestCmp.tsx
+++ src/TestCmp.tsx
@@ -1,7 +1,44 @@
 import * as React from "react";
 
-export default class extends React.Component {
+interface Users {
+  id: number;
+  name: string;
+  username: string;
+}
+interface State {
+  users: Users[] | null;
+}
+interface Props {}
+export default class extends React.Component<Props, State> {
+  constructor() {
+    super();
+
+    this.state = {
+      users: [
+        {
+          id: 1,
+          name: "James Armstrong",
+          username: "JArmstrong"
+        },
+        {
+          id: 2,
+          name: "John Doe",
+          username: "JDoe"
+        },
+        {
+          id: 3,
+          name: "John Smith",
+          username: "JSmith"
+        }
+      ]
+    };
+  }
   render() {
-    return <div>Loading...</div>;
+    const { users } = this.state;
+    return !users ? (
+      <div>Loading...</div>
+    ) : (
+      <ul>{users.map(u => <li key={u.id}>{u.name}</li>)}</ul>
+    );
   }
 }
