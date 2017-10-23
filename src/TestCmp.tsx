8e99704db22f5b48a0a0053c2444ab77e4ca4d64
James Armstrong <intellemerc@James-Macbook.lan>
2017-10-22 23:26:55 -0400

    add some comments and a proprs sample(limit)

--- src/TestCmp.tsx
+++ src/TestCmp.tsx
@@ -11,7 +11,9 @@ interface User {
 interface State {
   users: User[] | null;
 }
-interface Props {}
+interface Props {
+  limit: number;
+}
 
 const UserInfo = (user: User) => (
   <li className="user">
@@ -30,26 +32,32 @@ export default class extends React.Component<Props, State> {
     };
   }
   componentDidMount() {
+    // make an ajax request to the URL and return a promise
     fetch(dataUrl)
       .then(data => {
+        // convert the ajax response to a promise that returns json
         return data.json();
       })
       .then(data => {
+        // after results are returned update the components
+        // state to the list of users
         this.setState({
           users: data
         });
       })
       .catch(reason => {
+        // some error handling, have to disable ts lint to use console
         // tslint:disable-next-line:no-console
         console.log(reason);
       });
   }
   render() {
     const { users } = this.state;
+    const { limit } = this.props;
     return !users ? (
       <div>Loading...</div>
     ) : (
-      <ul>{users.map(u => <UserInfo key={u.id} {...u} />)}</ul>
+      <ul>{users.slice(0, limit).map(u => <UserInfo key={u.id} {...u} />)}</ul>
     );
   }
 }
